35e6093e7af573719b0ea76258daf6d49292a524
zzoingsu <nininick@naver.com>
2026-04-06 18:28:43 +0900

    2026.04.06 Java-Basic-Study

---
+++ src/ch08/sec04/Audio.java
@@ -0,0 +1,30 @@
+package ch08.sec04;
+
+public class Audio implements RemoteControl {
+private int volume;
+	
+	@Override
+	public void turnOn() {
+		System.out.println("TV를 켭니다");
+	}
+	
+	@Override
+	public void turnOff() {
+		System.out.println("TV를 끕니다");
+	}
+	
+	@Override
+	public void setVolume(int volum) {
+		if (volum > RemoteControl.MAX_VOLUME) {
+			this.volume = RemoteControl.MAX_VOLUME;
+		} else if ( volum < RemoteControl.MIN_VOLUME) {
+			this.volume = RemoteControl.MIN_VOLUME;
+		} else {
+			this.volume = volume;
+		}
+		System.out.println("현재 TV 볼륨: " + this.volume);
+	}
+	
+
+
+}
