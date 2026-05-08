78fc7495acb5e98059d67f33da0d4b34b2425ef9
zzoingsu <nininick@naver.com>
2026-05-08 17:55:12 +0900

    2026.05.08 Java-Basic-Study

--- Java-Basic-Problems/ch12/javaBaseModule04_MemberInfo/Member.java
+++ Java-Basic-Problems/ch12/javaBaseModule04_MemberInfo/Member.java
@@ -4,8 +4,9 @@ public class Member {
 	private String id;
 	private String name;
 	private int score;
+	private String grade;
 	
-	public Member(String id, String name, int score) {
+	public Member(String id, String name, int score, String grade) {
 		this.id = id;
 		this.name = name;
 		this.score = score;
@@ -23,10 +24,25 @@ public class Member {
 		return score;
 	}
 	
-	public void printInfo(String[] dataInfo) {
-		for(int i=0; i< dataInfo.length; i++) {
+	public String getGrade() {
+		return grade;
+	}
+	
+	public String gradeScope(int score) {
+			if(score > 90) {
+				return "A";
+			}else if(score > 80) {
+				return "B";
+			}else if(score > 70) {
+				return "C";
+			}else {
+				return "D";
 			
-		}
+		} 
+	}
+	@Override
+	public String toString() {
+		return "회원ID: " + id + ", 이름: " + name + ", 점수: " + score + ", 등급: " + grade;
 	}
 
 }
--- Java-Basic-Problems/ch12/javaBaseModule04_MemberInfo/MemberInfo.java
+++ Java-Basic-Problems/ch12/javaBaseModule04_MemberInfo/MemberInfo.java
@@ -4,15 +4,15 @@ public class MemberInfo {
 
 	public static void main(String[] args) {
 		String data = "user01:홍길동:85,user02:김자바:92,user03:이코딩:77,user04:박테스트:100";
-		Member member;
 		String[] array = data.split(",");
+		Member[] members = new Member[array.length];
+		String grade = null;
+		
 		for (int i=0; i<array.length; i++) {
 			String[] dataInfo = array[i].split(":");
-			String id = dataInfo[0];
-			String name = dataInfo[1];
 			int score = Integer.parseInt(dataInfo[2]);
-			System.out.println(score);
-			member = new Member(dataInfo[0], dataInfo[1], score);
+			grade = members[i].gradeScope(score);
+			members[i] = new Member(dataInfo[0], dataInfo[1], score, grade);
 		}
 		
 	}
