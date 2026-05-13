31a480b1b9f4374f79c1a427b062788f77d25e54
zzoingsu <nininick@naver.com>
2026-05-13 17:03:11 +0900

    2026.05.13 Java-Basic-Study

---
+++ src/ch14/sec05/exam02/JoinExample.java
@@ -0,0 +1,15 @@
+package ch14.sec05.exam02;
+
+public class JoinExample {
+
+	public static void main(String[] args) {
+		SumThread sumThread = new SumThread();
+		sumThread.start();
+		try {
+			sumThread.join();
+		} catch (Exception e) {
+		}
+		System.out.println("1~100 합: " + sumThread.getSum());
+	}
+
+}
