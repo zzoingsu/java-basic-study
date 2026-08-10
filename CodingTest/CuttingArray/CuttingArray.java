c359ce31b586b5c1eb89ac3af11e19daf32b78d5
zzoingsu <nininick@naver.com>
2026-08-10 18:38:45 +0900

    2026.08.10 CodingTest

--- CodingTest/CuttingArray/CuttingArray.java
+++ CodingTest/CuttingArray/CuttingArray.java
@@ -5,16 +5,12 @@ import java.util.List;
 
 public class CuttingArray {
 	class Solution {
-	    public List<Integer> solution(int n, long left, long right) {
-	    	List<Integer> value = new ArrayList<>();
-	    	int count = 0;
-	    	for(int i=1; i<=n; i++) {
-	    		for(int j=1; j<=n; j++) {
-	    			count++;
-	    			if(count>=left && count <= right) {
-	    				value.add(Math.max(i, j));
-	    			}
-	    		}
+	    public List<Long> solution(int n, long left, long right) {
+	    	List<Long> value = new ArrayList<Long>();
+	    	for(Long i= left; i<=right; i++) {
+	    		Long row = i/n;
+		    	Long colum = i%n;
+		    	value.add(Math.max(row, colum));
 	    	}
 	    	return value;
 	    }
---
+++ CodingTest/LongJump/LongJump.java
@@ -0,0 +1,5 @@
+package LongJump;
+
+public class LongJump {
+
+}
