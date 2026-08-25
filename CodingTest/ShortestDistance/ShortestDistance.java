34c9aa9f77128d5c22c530cad19a6d40c68ad8f5
zzoingsu <nininick@naver.com>
2026-08-25 22:36:13 +0900

    CodingTest

--- CodingTest/ShortestDistance/ShortestDistance.java
+++ CodingTest/ShortestDistance/ShortestDistance.java
@@ -1,10 +1,12 @@
 package ShortestDistance;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ShortestDistance {
 	class Solution {
 	    public int solution(int[][] maps) {
-	    	boolean[][] visited = new boolean[maps.length][maps[0].length];
-	        return dfs(maps, 0, 0, visited, 0);
+	    	return bfs(maps);
 	    }
 	    public static int dfs(int[][]maps, int length, int width, boolean[][] visited,int count) {
 	    	int min=100000;
@@ -29,6 +31,36 @@ public class ShortestDistance {
 	    	int upDown = Math.min(down, up);
 	    	int leftRight = Math.min(left, right);
 	    	return Math.min(upDown, leftRight);
+	    }
+	    public static int bfs(int[][] maps) {
+	    	Queue<int[]> queue = new LinkedList<>();
+	    	int count = 0;
+	    	queue.add(new int[] {0, 0});
+	    	while(!queue.isEmpty()) {
+	    		int[] location = queue.poll();
+	    		int length = location[0];
+	    		int width = location[1];
+	    		if(location[0] == maps.length-1 && location[1] == maps[0].length-1) {
+	    			return count;
+	    		}
+	    		if(length+1 < maps.length && maps[length+1][width] != 0) {
+	    			queue.add(new int[] {length+1, width});
+	    			count ++;
+	    		}
+	    		if(length-1>=0 && maps[length-1][width] != 0) {
+	    			queue.add(new int[] {length-1, width});
+	    			count ++;
+	    		}
+	    		if(width+1 < maps[0].length && maps[length][width+1] != 0) {
+	    			queue.add(new int[] {length, width+1});
+	    			count ++;
+	    		}
+	    		if(width > 0 && maps[length][width-1] != 0) {
+	    			queue.add(new int[] {length, width-1});
+	    			count ++;
+	    		}
+	    	}
+	    	return -1;
 	    }
 	}
 }
