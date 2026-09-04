148e0f6532101704bf1f1fdede35ef4d7424083f
zzoingsu <nininick@naver.com>
2026-09-04 22:31:16 +0900

    CodingTest

--- CodingTest/SplitThePowerGridInTwo/SplitThePowerGridInTwo.java
+++ CodingTest/SplitThePowerGridInTwo/SplitThePowerGridInTwo.java
@@ -2,37 +2,50 @@ package SplitThePowerGridInTwo;
 
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class SplitThePowerGridInTwo {
 	class Solution {
 	    public int solution(int n, int[][] wires) {
-	        int answer = -1;
-	        return answer;
+	        return cut(wires, n);
 	    }
-	    public static int spilt(int[][] wires, int n) {
-	    	Queue<int[]> queue = new LinkedList<>(Arrays.asList(wires));
-	    	int count = 0;
-	    	while(count == n-1) {
-	    		int[] grid = queue.poll();
+	    public static int cut(int[][] wires, int n) {
+	    	List<int[]> wire = new LinkedList<>(Arrays.asList(wires));
+	    	int min = 1000;
+	    	for(int i=0; i<wire.size(); i++) {
+	    		boolean[] visited = new boolean[wire.size()];
+	    		int[] grid = wire.get(i);
+	    		wire.remove(i);
 	    		int start = grid[0];
+	    		int startNod = dfs(wire, visited, start);
+	    		
 	    		int end = grid[1];
-	    		count++;
+	    		int endNod = dfs(wire, visited, end);
+	    		
+	    		int result = Math.abs(startNod-endNod);
+	    		min = Math.min(result, min);
+	    		wire.add(i, grid);
 	    	}
-	    	return 0;
+	    	return min;
 	    }
-	    public static int countNods(Queue<int[]> queue, int nod) {
-	    	int count = 0;
-	    	int[] wire = queue.poll();
-	    	if(nod == wire[0]) {
-	    		nod = wire[0];
-	    		count++;
-	    	}
-	    	if(nod == wire[1]) {
-	    		nod = wire[1];
-	    		count++;
-	    	}
-	    	return count;
+	    public static int dfs(List<int[]> wire, boolean[] visited, int nod) {
+	        int count = 0;
+	        for (int i = 0; i < wire.size(); i++) {
+	            if (nod == wire.get(i)[0] && !visited[i]) {
+	                int nextNod = wire.get(i)[1];
+	                visited[i] = true;
+	                count += 1;
+	                count += dfs(wire, visited, nextNod);
+	            }
+	            if (nod == wire.get(i)[1] && !visited[i]) {
+	                int nextNod = wire.get(i)[0];
+	                visited[i] = true;
+	                count += 1;
+	                count += dfs(wire, visited, nextNod);
+	            }
+	        }
+	        return count;
 	    }
 	}
 }
